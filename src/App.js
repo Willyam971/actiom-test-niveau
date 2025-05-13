e634dad43f629d8d53889a49a21d1193b5460a05
Willyam971 <sytadin29@gmail.com>
2025-05-13 16:15:36 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Test de niveau Actiom</h1>
+      <p>Bienvenue sur le test. (Le vrai contenu arrive ensuite)</p>
+    </div>
+  );
+}
